16a002d13dee59ba57b6b03d713e22b135bba860
Brian <bwood@berkeley.edu>
2024-11-12 22:03:50 +0100

    OpenSCAD: Fix scad-command. Add scad-indent-level.

--- custom.el
+++ custom.el
@@ -3,8 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files
-   '("/Users/bwood/Documents/orgmode2/agenda/wps.org" "/Users/bwood/Documents/orgmode2/agenda/admin.org" "/Users/bwood/Documents/orgmode2/agenda/emacs.org" "/Users/bwood/Documents/orgmode2/agenda/open_berkeley.org" "/Users/bwood/Documents/orgmode2/agenda/system_maintenance.org")))
+ '(ignored-local-variable-values '((git-commit-major-mode . git-commit-elisp-text-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
